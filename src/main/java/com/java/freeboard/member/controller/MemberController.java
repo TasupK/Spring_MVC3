d4f3d2ebc0822484cc56326a062e64730aee03d5
LAPTOP-FB00KKA5\Tsup <ruyzlen@gmail.com>
2021-03-14 18:00:29 +0900

    2021-03-14

--- src/main/java/com/java/freeboard/member/controller/MemberController.java
+++ src/main/java/com/java/freeboard/member/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.java.freeboard.member.service.MemberService;
 import com.java.freeboard.vo.MemberVO;
 
+
 @Controller
 @RequestMapping("/member/*")
 public class MemberController {
@@ -17,13 +18,21 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	
-	
+	/**
+	 * 로그인 페이지 
+	 * @author Tsup
+	 *
+	 */
 	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
 	public String login() {
 		return "/member/login_page";
 	}
 	
-	
+	/**
+	 * 로그인 처리 프로세스 
+	 * @author Tsup
+	 *
+	 */
 	@RequestMapping(value = "/login_process.do", method = RequestMethod.GET)
 	public String loginProcess(MemberVO memberVO, HttpSession session) {
 		MemberVO userData = memberService.getMemberByIdAndPw(memberVO);
@@ -31,22 +40,36 @@ public class MemberController {
 			return "/member/login_fail";
 		}else {
 			session.setAttribute("sessionUserData", userData);
-			System.out.println("세션에 들어있는 값 : "+session.getAttribute("sessionUserData").toString());
 			return "redirect:/board/main_page.do";
 		}
 	}
 	
+	/**
+	 * 로그인 아웃 프로세스 
+	 * @author Tsup
+	 *
+	 */
 	@RequestMapping("/logout_process.do")
 	public String logoutProcess(HttpSession session) {
 		session.invalidate();
 		return "redirect:/member/login.do";
 	}
 	
+	/**
+	 * 회원가입 페이지 
+	 * @author Tsup
+	 *
+	 */
 	@RequestMapping("/join_member_page.do")
 	public String joinMember() {
 		return "/member/join_member_page";
 	}
 	
+	/**
+	 * 회원가입 프로세스 
+	 * @author Tsup
+	 *
+	 */
 	@RequestMapping("/join_member_process.do")
 	public String joinMemberProcess(MemberVO memberVO) {
 		memberService.insertMember(memberVO);
